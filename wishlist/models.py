d0e01bc4fac3a0ae28b606f80617beb271c58449
daffafaizan <112247545+daffafaizan@users.noreply.github.com>
2022-09-08 15:56:22 +0700

    Initial Commit

---
+++ wishlist/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class BarangWishlist(models.Model):
+    nama_barang = models.CharField(max_length=50)
+    harga_barang = models.IntegerField()
+    deskripsi = models.TextField()
---
+++ wishlist/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+# Create your views here.
+def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Daffa'
+    }
+    return render(request, "wishlist.html", context)
